818976233fea08b9d7a48ccb714765af459952a6
Dan Colish <dcolish@gmail.com>
2012-07-06 23:56:57 -0700

    Clean up parser code and PEP8, Use dispatcher for handlers

--- braces/parser.py
+++ braces/parser.py
@@ -7,6 +7,8 @@ This module is only meant for internal use by the renderengine module.
 
 """
 
+#TODO:dc: Reimplement in C
+
 import re
 
 from braces.common import TemplateNotFoundError
@@ -35,6 +37,8 @@ def _compile_template_re(delimiters=None):
     #   The tag's content MUST be a non-whitespace character sequence
     #   NOT containing the current closing delimiter.
     #
+    # otag is opening delimiter
+    # ctag is closing delimiter
     tag = r"""
         (?P<whitespace>[\ \t]*)
         %(otag)s \s*
@@ -44,7 +48,9 @@ def _compile_template_re(delimiters=None):
           (?P<tag>[%(tag_types)s]?)  \s* (?P<tag_key>[\s\S]+?)
         )
         \s* %(ctag)s
-    """ % {'tag_types': tag_types, 'otag': re.escape(delimiters[0]), 'ctag': re.escape(delimiters[1])}
+    """ % {'tag_types': tag_types,
+           'otag': re.escape(delimiters[0]),
+           'ctag': re.escape(delimiters[1])}
 
     return re.compile(tag, re.VERBOSE)
 
@@ -100,7 +106,7 @@ class Parser(object):
         """
         parse_tree = []
         index = start_index
-
+        template_length = len(template)
         while True:
             match = self._template_re.search(template, index)
 
@@ -110,6 +116,7 @@ class Parser(object):
             match_index = match.start()
             end_index = match.end()
 
+            # All text before the first delimiter match
             before_tag = template[index : match_index]
 
             parse_tree.append(before_tag)
@@ -117,9 +124,9 @@ class Parser(object):
             matches = match.groupdict()
 
             # Normalize the matches dictionary.
-            if matches['change'] is not None:
+            if matches['change']:
                 matches.update(tag='=', tag_key=matches['delims'])
-            elif matches['raw'] is not None:
+            elif matches['raw']:
                 matches.update(tag='&', tag_key=matches['raw_name'])
 
             tag_type = matches['tag']
@@ -128,15 +135,22 @@ class Parser(object):
 
             # Standalone (non-interpolation) tags consume the entire line,
             # both leading whitespace and trailing newline.
-            did_tag_begin_line = match_index == 0 or template[match_index - 1] in END_OF_LINE_CHARACTERS
-            did_tag_end_line = end_index == len(template) or template[end_index] in END_OF_LINE_CHARACTERS
+            did_tag_begin_line = did_tag_end_line = False
+            if (match_index == 0 or
+                template[match_index - 1] in END_OF_LINE_CHARACTERS):
+                did_tag_begin_line = True
+
+            if (end_index == template_length or
+                template[end_index] in END_OF_LINE_CHARACTERS):
+                did_tag_end_line = True
+
             is_tag_interpolating = tag_type in ['', '&']
 
-            if did_tag_begin_line and did_tag_end_line and not is_tag_interpolating:
-                if end_index < len(template):
-                    end_index += template[end_index] == '\r' and 1 or 0
-                if end_index < len(template):
-                    end_index += template[end_index] == '\n' and 1 or 0
+            if (did_tag_begin_line and did_tag_end_line and
+                not is_tag_interpolating):
+                if (end_index < template_length and
+                    template[end_index] in END_OF_LINE_CHARACTERS):
+                    end_index += 1
             elif leading_whitespace:
                 parse_tree.append(leading_whitespace)
                 match_index += len(leading_whitespace)
@@ -144,11 +158,14 @@ class Parser(object):
 
             if tag_type == '/':
                 if tag_key != section_key:
-                    raise ParsingError("Section end tag mismatch: %s != %s" % (tag_key, section_key))
+                    raise ParsingError("Section end tag mismatch: %s != %s" % (
+                            tag_key, section_key))
 
                 return ParsedTemplate(parse_tree), match_index, end_index
 
-            index = self._handle_tag_type(template, parse_tree, tag_type, tag_key, leading_whitespace, end_index)
+            index = self._handle_tag_type(template, parse_tree,
+                                          tag_type, tag_key,
+                                          leading_whitespace, end_index)
 
         # Save the rest of the template.
         parse_tree.append(template[index:])
@@ -183,39 +200,29 @@ class Parser(object):
 
         return parsed_section, template[start_index:content_end_index], end_index
 
-    def _handle_tag_type(self, template, parse_tree, tag_type, tag_key, leading_whitespace, end_index):
+    def _handle_tag_type(self, template, parse_tree, tag_type, tag_key,
+                         leading_whitespace, end_index):
 
         # TODO: switch to using a dictionary instead of a bunch of ifs and elifs.
-        if tag_type == '!':
-            return end_index
-
-        if tag_type == '=':
-            delimiters = tag_key.split()
-            self._change_delimiters(delimiters)
-            return end_index
-
         engine = self.engine
 
-        if tag_type == '':
+        def _handle_change(end_index):
+            delimiters = tag_key.split()
+            self._change_delimiters(delimiters)
+            return end_index, None
 
-            func = engine._make_get_escaped(tag_key)
+        def _handle_hash(end_index):
+            parsed_section, section_contents, end_index = self._parse_section(
+                template, end_index, tag_key)
+            return end_index, engine._make_get_section(
+                tag_key, parsed_section, section_contents, self._delimiters)
 
-        elif tag_type == '&':
+        def _handle_caret(end_index):
+            parsed_section, section_contents, end_index = self._parse_section(
+                template, end_index, tag_key)
+            return end_index, engine._make_get_inverse(tag_key, parsed_section)
 
-            func = engine._make_get_literal(tag_key)
-
-        elif tag_type == '#':
-
-            parsed_section, section_contents, end_index = self._parse_section(template, end_index, tag_key)
-            func = engine._make_get_section(tag_key, parsed_section, section_contents, self._delimiters)
-
-        elif tag_type == '^':
-
-            parsed_section, section_contents, end_index = self._parse_section(template, end_index, tag_key)
-            func = engine._make_get_inverse(tag_key, parsed_section)
-
-        elif tag_type == '>':
-
+        def _handle_gt(end_index):
             try:
                 # TODO: make engine.load() and test it separately.
                 template = engine.load_partial(tag_key)
@@ -223,15 +230,29 @@ class Parser(object):
                 template = u''
 
             # Indent before rendering.
-            template = re.sub(NON_BLANK_RE, leading_whitespace + ur'\1', template)
+            template = re.sub(
+                NON_BLANK_RE, leading_whitespace + ur'\1', template)
 
-            func = engine._make_get_partial(template)
+            return end_index, engine._make_get_partial(template)
 
-        else:
+        dispatch = {
+            '!': lambda x: (x, None),
+            '=': _handle_change,
+            '': lambda x: (end_index, engine._make_get_escaped(tag_key)),
+            '&': lambda x: (end_index, engine._make_get_literal(tag_key)),
+            '#': _handle_hash,
+            '^': _handle_caret,
+            '>': _handle_gt,
+            }
 
+        func = None
+        try:
+            end_index, func = dispatch[tag_type](end_index)
+        except KeyError:
             raise Exception("Unrecognized tag type: %s" % repr(tag_type))
 
+        if func is None:
+            return end_index
         parse_tree.append(func)
 
         return end_index
-
